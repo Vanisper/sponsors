1224147b2d1b359cb8d4aef6eb32279219535951
Vanisper <273266469@qq.com>
2025-01-04 03:55:13 +0800

    feat: 添加赞助者信息

--- custom-sponsors/index.ts
+++ custom-sponsors/index.ts
@@ -18,6 +18,40 @@ const sponsors: Sponsorship[] = [
     isOneTime: true,
     provider: "WeChat",
   },
+  {
+    sponsor: {
+      type: "User",
+      login: "asennaggmkl",
+      name: "阿森纳",
+      avatarUrl: "https://avatars.githubusercontent.com/u/178490476",
+      linkUrl: "https://github.com/asennaggmkl",
+    },
+    monthlyDollars: convertCNYToUSD(6.66 + 1 + 1),
+    privacyLevel: "PUBLIC",
+    tierName: "WeChat",
+    createdAt: "2025-01-03T10:03:16+08:00",
+    isOneTime: true,
+    provider: "WeChat",
+    raw: {
+      sponsors: [
+        {
+          cny: 6.66,
+          description: "必须吃辣条",
+          createdAt: "2025-01-03T10:03:16+08:00",
+        },
+        {
+          cny: 1,
+          description: "ComeOnMakeMoney",
+          createdAt: "2025-01-03T11:16:32+08:00",
+        },
+        {
+          cny: 1,
+          description: "您好 咨询问题",
+          createdAt: "2025-01-03T11:16:49+08:00",
+        },
+      ],
+    }
+  },
 ];
 
 export default sponsors;
